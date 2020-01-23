1a9ab36de24ff7b96314be836659e171b8bc9b1c
DennySPB <dennyspb@gmail.com>
2020-01-23 11:42:00 +0300

    SystemUI: TypeClock: adapt to recents clock keyguard changes
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: I6bff0044abb48606298fae10554e3a767c758af0
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/clock/TypeClockController.java
+++ packages/SystemUI/src/com/android/keyguard/clock/TypeClockController.java
@@ -54,11 +54,6 @@ public class TypeClockController implements ClockPlugin {
     private final SysuiColorExtractor mColorExtractor;
 
     /**
-     * Computes preferred position of clock.
-     */
-    private final SmallClockPosition mClockPosition;
-
-    /**
      * Renders preview from clock view.
      */
     private final ViewPreviewer mRenderer = new ViewPreviewer();
@@ -66,8 +61,8 @@ public class TypeClockController implements ClockPlugin {
     /**
      * Custom clock shown on AOD screen and behind stack scroller on lock.
      */
-    private View mView;
     private TypographicClock mTypeClock;
+    private ClockLayout mBigClockView;
 
     /**
      * Small clock shown on lock screen above stack scroller.
@@ -106,27 +101,18 @@ public class TypeClockController implements ClockPlugin {
         mResources = res;
         mLayoutInflater = inflater;
         mColorExtractor = colorExtractor;
-        mClockPosition = new SmallClockPosition(res);
         mContext = context;
     }
 
     private void createViews() {
-        mView = mLayoutInflater.inflate(R.layout.type_aod_clock, null);
-        mTypeClock = mView.findViewById(R.id.type_clock);
-
-        // For now, this view is used to hide the default digital clock.
-        // Need better transition to lock screen.
-        mLockClock = (TypographicClock) mLayoutInflater.inflate(R.layout.typographic_clock, null);
-        mLockClock.setVisibility(View.GONE);
-
-        mDarkController = new CrossFadeDarkController(mView, mLockClock);
+        mBigClockView  = (ClockLayout) mLayoutInflater.inflate(R.layout.typographic_clock, null);
+        mTypeClock = mBigClockView.findViewById(R.id.type_clock);
     }
 
     @Override
     public void onDestroyView() {
-        mView = null;
+        mBigClockView = null;
         mTypeClock = null;
-        mLockClock = null;
         mDarkController = null;
     }
 
@@ -164,32 +150,25 @@ public class TypeClockController implements ClockPlugin {
 
     @Override
     public View getView() {
-        if (mLockClock == null) {
-            createViews();
-        }
-        return mLockClock;
+        return null;
     }
 
     @Override
     public View getBigClockView() {
-        if (mView == null) {
+        if (mBigClockView == null) {
             createViews();
         }
-        return mView;
+        return mBigClockView;
     }
 
     @Override
     public int getPreferredY(int totalHeight) {
-        return mClockPosition.getPreferredY();
+        return totalHeight / 2;
     }
-
-    @Override
-    public void setStyle(Style style) {}
 
     @Override
     public void setTextColor(int color) {
         mTypeClock.setTextColor(color);
-        mLockClock.setTextColor(color);
     }
 
     @Override
@@ -199,32 +178,26 @@ public class TypeClockController implements ClockPlugin {
         }
         final int color = colorPalette[Math.max(0, colorPalette.length - 5)];
         mTypeClock.setClockColor(color);
-        mLockClock.setClockColor(color);
     }
 
     @Override
     public void onTimeTick() {
         mTypeClock.onTimeChanged();
-        mLockClock.onTimeChanged();
     }
 
     @Override
     public void setDarkAmount(float darkAmount) {
-        mClockPosition.setDarkAmount(darkAmount);
-        if (mDarkController != null) {
-            mDarkController.setDarkAmount(darkAmount);
-        }
+        mBigClockView.setDarkAmount(darkAmount);
     }
 
     @Override
     public void onTimeZoneChanged(TimeZone timeZone) {
         mTypeClock.onTimeZoneChanged(timeZone);
-        mLockClock.onTimeZoneChanged(timeZone);
     }
 
     @Override
     public boolean shouldShowStatusArea() {
         if (mContext == null) return true;
-        return Settings.System.getInt(mContext.getContentResolver(), Settings.System.CLOCK_SHOW_STATUS_AREA, 0) == 1;
+        return Settings.System.getInt(mContext.getContentResolver(), Settings.System.CLOCK_SHOW_STATUS_AREA, 1) == 1;
     }
 }
