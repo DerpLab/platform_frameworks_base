cc386c220de1512a17ed651e4f6f6cdd5a6a1125
SuperDroidBond <superdroidbond@yahoo.com>
2020-02-08 21:16:03 +0530

    base: Grant storage permission to Lawnchair
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -846,6 +846,9 @@ public final class DefaultPermissionGrantPolicy {
         // Google Markup
         grantSystemFixedPermissionsToSystemPackage("com.google.android.markup", userId, STORAGE_PERMISSIONS);
 
+        // Lawnchair
+        grantSystemFixedPermissionsToSystemPackage("ch.deletescape.lawnchair.ci", userId, STORAGE_PERMISSIONS);
+
         // Google Recorder
         grantSystemFixedPermissionsToSystemPackage("com.google.android.apps.recorder", userId, MICROPHONE_PERMISSIONS);
 
