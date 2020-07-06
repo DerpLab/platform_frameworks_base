6c4f41bbde924809d31013e2bac80689c686462d
Kshitij Gupta <kshitijgm@gmail.com>
2020-07-06 22:57:28 +0530

    base: Remove current immserse corner impl
    - Sync with AOSP ScreenDecorations for now; Lets do this correctly, for
      a change.
    
    Change-Id: Iff7cf80580652fefd9c3542ec496cd9e05acc83d

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -2552,7 +2552,6 @@ public class NotificationPanelView extends PanelView implements
 
     @Override
     protected void onTrackingStarted() {
-        Secure.putInt(mContext.getContentResolver(), "sysui_rounded_size_top", -1);
         mFalsingManager.onTrackingStarted(mStatusBar.isKeyguardCurrentlySecure());
         super.onTrackingStarted();
         if (mQsFullyExpanded) {
@@ -2568,8 +2567,6 @@ public class NotificationPanelView extends PanelView implements
 
     @Override
     protected void onTrackingStopped(boolean expand) {
-        if (!expand)
-            Secure.putInt(mContext.getContentResolver(), "sysui_rounded_size_top", -2);
         mFalsingManager.onTrackingStopped();
         super.onTrackingStopped(expand);
         if (expand) {
